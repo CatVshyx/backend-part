9633d11ddf56cc557d09ca1b6c061f80d4af2e58
Dmitro <47032134+CatVshyx@users.noreply.github.com>
2024-03-22 01:45:38 +0200

    added new files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT   INTO type(id, name,icon) VALUES (1,'Перелік НПА','fa-light fa-book');
+INSERT  INTO type(id, name,icon) VALUES (2,'Конституція України','fa-light fa-scale-balanced');
+INSERT  INTO type(id, name,icon) VALUES (3,'Кодекси','fa-light fa-books');
+INSERT  INTO type(id, name,icon) VALUES (4,'Закони','fa-light fa-landmark');
+INSERT  INTO type(id, name,icon) VALUES (5,'Рішення Пленуму ВС','fa-light fa-pen-to-square') ;
+INSERT  INTO type(id, name,icon) VALUES (6,'Накази та Інструкції','fa-light fa-memo-circle-info');
+
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(1,'Заходи ЗКП','Заходи забезпечення кримінального провадження','fa-light fa-book-bookmark',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(2,'Початковий','Процесуальні документи','fa-light fa-1',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(3,'Подальший','Процесуальні документи','fa-light fa-2',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(4,'Заключний','Процесуальні документи','fa-light fa-3',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(5,'Прокурор (Процес. керівник)','Прокурор на стадії досудового розслідування','fa-light fa-person-military-pointing',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(6,'Судові експертизи',null,'fa-light fa-pen-to-square',0);
+INSERT  INTO category(id,name,description,icon,last_number) VALUES(7,'НСРД','Негласні слідчі (розшукові) дії','fa-light fa-user-secret',0);
+
